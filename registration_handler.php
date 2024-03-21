f5b585bab3af5c564c6efe92f98d658bf258c9fd
athumani <athumanshukuru33@gmail.com>
2024-03-21 16:22:26 +0300

    done registration page and its php handler

---
+++ registration_handler.php
@@ -0,0 +1,14 @@
+<?php
+
+// Get the submitted form data
+$firstName = $_POST['first_name'];
+$lastName = $_POST['last_name'];
+$dateOfBirth = $_POST['date_of_birth'];
+$nationality = $_POST['nationality'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phone_number'];
+$program = $_POST['program'];
+$previousSchool = $_POST['previous_school'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+$emergencyContactName = $_POST['emergency_contact'];
